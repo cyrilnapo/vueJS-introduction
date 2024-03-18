b5a861108850e249ea997983c6c7640c048e4e3f
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-03-18 09:32:30 +0100

    l4 start

--- code/main.js
+++ code/main.js
@@ -1,9 +1,8 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      product: "Socks",
-      image: "./assets/images/socks_green.jpg",
-      url: " https://www.etml.ch/",
-    };
-  },
-});
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg'
+        }
+    }
+})
