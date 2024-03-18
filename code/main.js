ff9fa99d19b7719682012eea06c27d4d1d39ec68
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-03-18 10:24:23 +0100

    l5 start

--- code/main.js
+++ code/main.js
@@ -1,11 +1,10 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      product: "Socks",
-      image: "./assets/images/socks_blue.jpg",
-      inStock: true,
-      inventory: 100,
-      onSale: true,
-    };
-  },
-});
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester']
+        }
+    }
+})
